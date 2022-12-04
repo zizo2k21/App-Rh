cdd745751338f894bbd8cfcd4e94a79bbe0e4a08
zizo2k21 <yedjoussigue.kone@yahoo.fr>
2022-12-04 20:53:56 +0100

    Debut portfolio

--- app-rh/src/App.js
+++ app-rh/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import "./scss/style.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './views/home';
 
